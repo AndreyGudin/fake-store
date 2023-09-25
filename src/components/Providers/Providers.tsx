c779a66c5033b8656d9743dfd9a8fa0aae487e88
Andrey Gudin <aeon150791@gmail.com>
2023-09-25 13:34:49 +0400

    feat: add component providers with tanstack queryclientprovider

---
+++ src/components/Providers/Providers.tsx
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { FC, ReactNode } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+const queryClient = new QueryClient();
+
+export const Providers: FC<ProvidersProps> = ({ children }: ProvidersProps) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
