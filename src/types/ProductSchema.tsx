1d9e2b7eb4a5630549befc9f6d8387a6c55df460
Andrey Gudin <aeon150791@gmail.com>
2023-09-28 10:40:45 +0400

    feat: add zod object for product

--- src/types/ProductSchema.tsx
+++ src/types/ProductSchema.tsx
@@ -1,9 +1,13 @@
-export interface ProductSchema {
-  id: number;
-  title: string;
-  price: number;
-  category: string;
-  description: string;
-  image: string;
-  rating: { rate: number; count: number };
-}
+import { z } from "zod";
+
+export const ProductSchema2 = z.object({
+  id: z.number(),
+  title: z.string(),
+  price: z.number(),
+  category: z.string(),
+  description: z.string(),
+  image: z.string(),
+  rating: z.object({ rate: z.number(), count: z.number() }),
+});
+
+export type ProductSchema = z.infer<typeof ProductSchema2>;
