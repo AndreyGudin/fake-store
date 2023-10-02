7a118d4659da75fae13473211eef473c852bc7f6
Andrey Gudin <aeon150791@gmail.com>
2023-10-02 16:39:00 +0400

    feat: add middleware for route guard

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,5 +3,10 @@ import { Header } from "@/components/Header";
 import { Products } from "@/components/Products/Products";
 
 export default function Home() {
-  return <main className='text-3xl'>page</main>;
+  return (
+    <main className='text-3xl'>
+      <Header />
+      <Products />
+    </main>
+  );
 }
---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const secToken = "eyJhbGciOiJIUzI1NiIsInR";
+
+export default async function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+  if (token) return NextResponse.next();
+
+  const url = req.nextUrl.clone();
+  url.pathname = "/login";
+  return NextResponse.redirect(url);
+}
+
+export const config = {
+  matcher: ["/products", "/"],
+};
