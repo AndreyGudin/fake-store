40af5acdf6da29c0cdf510d8158d082086f3b18a
Andrey Gudin <aeon150791@gmail.com>
2024-07-29 19:25:36 +0400

    refactor: delete deprecated

--- src/deprecated/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/deprecated/LoginForm";
-
-export default async function Page({ params }: { params: { id: string } }) {
-  return (
-    <main className='w-full min-h-screen flex justify-center items-center'>
-      <LoginForm />
-    </main>
-  );
-}
--- src/deprecated/store/useIsAuthStore.tsx
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-import { IsAuthState } from "@/types/State";
-
-export const useIsAuthStore = create<IsAuthState>()((set) => ({
-  isAuth: false,
-  setIsAuth: (current) => set(() => ({ isAuth: current })),
-}));
