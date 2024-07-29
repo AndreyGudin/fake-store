d38adffd61785b7b48fde6725a507ef296e31f16
Andrey Gudin <aeon150791@gmail.com>
2024-07-29 11:07:36 +0400

    feat: remove login page and authenication

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import { Button } from "@/components/Button";
-import { Label } from "@/components/Label";
-import { useIsAuthStore } from "@/hooks/store/useIsAuthStore";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { memo, useCallback } from "react";
+import { memo } from "react";
 import type { FC } from "react";
 
 interface HeaderProps {
@@ -15,14 +12,6 @@ interface HeaderProps {
 export const Header: FC<HeaderProps> = memo(function Header({
   className = "",
 }: HeaderProps) {
-  const isAuth = useIsAuthStore((state) => state.isAuth);
-  const router = useRouter();
-
-  const handleLogOut = useCallback(() => {
-    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
-    router.push("/login");
-  }, [router]);
-
   return (
     <menu
       className={`${className} h-[100px] w-full flex justify-between items-center`}
@@ -30,16 +19,6 @@ export const Header: FC<HeaderProps> = memo(function Header({
       <Link href={"/create"}>
         <Button variant={"default"}>Create product</Button>
       </Link>
-      {isAuth && (
-        <div className='flex gap-4 items-center'>
-          <Label> You are authenticated</Label>
-          <Link href={"/login"}>
-            <Button onClick={handleLogOut} variant={"destructive"}>
-              Log out
-            </Button>
-          </Link>
-        </div>
-      )}
     </menu>
   );
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
-  const token = req.cookies.get("token")?.value;
   let url = req.nextUrl.clone();
 
-  if (token && req.nextUrl.pathname !== "/login") {
-    return NextResponse.next();
-  }
-  if (token && req.nextUrl.pathname === "/login") {
-    url.pathname = "/products";
-    return NextResponse.redirect(url);
-  }
-  if (req.nextUrl.pathname !== "/login") {
+  if (req.nextUrl.pathname === "/") {
     url = req.nextUrl.clone();
-    url.pathname = "/login";
+    url.pathname = "/products";
     return NextResponse.redirect(url);
   }
 }
 
 export const config = {
-  matcher: ["/products", "/", "/login"],
+  matcher: ["/"],
 };
